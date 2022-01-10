d453bf570ad7936dda68a086b11ceabcf5cd0a10
kristli <kristli@tencent.com>
2022-01-10 16:22:54 +0800

    chore: 移除多余的console.log

--- build/bin/TDIconInit.js
+++ build/bin/TDIconInit.js
@@ -8,6 +8,6 @@ const files = glob.sync(`${SRC_ICONS_DIR}/**/*.js`);
 
 const icons = files.map(file => path.basename(file).replace('.js', ''));
 
-console.log(icons);
+// console.log(icons);
 
 fs.writeFile(path.resolve(__dirname, '../../examples/icon_td.json'), JSON.stringify(icons), () => {});
