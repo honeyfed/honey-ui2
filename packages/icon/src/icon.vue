e23989a62c66750e428ae4454ad8ee3f4e139863
kristli <kristli@tencent.com>
2022-01-18 15:02:02 +0800

    fix(icon): click事件修正

--- packages/icon/src/icon.vue
+++ packages/icon/src/icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <icon v-if="tdName" :name="tdName" :size="size ? size : null" />
+  <icon v-if="tdName" :name="tdName" :size="size ? size : null" @click="tdIconClick" />
   <i v-else :class="'el-icon-' + name"></i>
 </template>
 
@@ -14,10 +14,13 @@ export default {
   props: {
     name: String,
     tdName: String,
-    size: String,
-    click: {
-      type: Function,
-      default: () => {}
+    size: String
+  },
+
+  methods: {
+    tdIconClick(e) {
+      // 代理 TD-Icon 组件自带的 click 事件，使其响应与标准点击事件回调一致
+      this.$listeners.click && this.$listeners.click(e.e || e);
     }
   }
 };
