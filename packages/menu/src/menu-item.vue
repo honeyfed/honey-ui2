2a579e71949b740fc10bb85f38d877956cb065d7
kristli <kristli@tencent.com>
2022-01-24 15:20:07 +0800

    feat(nav-menu): navMenu组件适配

--- packages/menu/src/menu-item.vue
+++ packages/menu/src/menu-item.vue
@@ -1,112 +1,130 @@
 <template>
-  <li class="el-menu-item"
+  <li
+    class="t-menu__item t-menu__item--plain"
     role="menuitem"
     tabindex="-1"
     :style="[paddingStyle, itemStyle, { backgroundColor }]"
     :class="{
-      'is-active': active,
-      'is-disabled': disabled
+      't-is-active': active,
+      't-is-disabled': disabled
     }"
     @click="handleClick"
     @mouseenter="onMouseEnter"
     @focus="onMouseEnter"
     @blur="onMouseLeave"
     @mouseleave="onMouseLeave"
+    v-ripple
   >
     <el-tooltip
       v-if="parentMenu.$options.componentName === 'ElMenu' && rootMenu.collapse && $slots.title"
       effect="dark"
-      placement="right">
+      placement="right"
+    >
       <div slot="content"><slot name="title"></slot></div>
-      <div style="position: absolute;left: 0;top: 0;height: 100%;width: 100%;display: inline-block;box-sizing: border-box;padding: 0 20px;">
+      <div
+        style="
+          position: absolute;
+          left: 0;
+          top: 0;
+          height: 100%;
+          width: 100%;
+          display: inline-block;
+          box-sizing: border-box;
+        "
+      >
         <slot></slot>
       </div>
     </el-tooltip>
     <template v-else>
       <slot></slot>
-      <slot name="title"></slot>
+      <span class="t-menu__content"><slot name="title"></slot></span>
     </template>
   </li>
 </template>
 <script>
-  import Menu from './menu-mixin';
-  import ElTooltip from 'element-ui/packages/tooltip';
-  import Emitter from 'element-ui/src/mixins/emitter';
+import Menu from './menu-mixin';
+import ElTooltip from 'element-ui/packages/tooltip';
+import Emitter from 'element-ui/src/mixins/emitter';
+import ripple from 'element-ui/src/utils/ripple';
 
-  export default {
-    name: 'ElMenuItem',
+export default {
+  name: 'ElMenuItem',
 
-    componentName: 'ElMenuItem',
+  componentName: 'ElMenuItem',
 
-    mixins: [Menu, Emitter],
+  mixins: [Menu, Emitter],
 
-    components: { ElTooltip },
+  directives: { ripple },
 
-    props: {
-      index: {
-        default: null,
-        validator: val => typeof val === 'string' || val === null
-      },
-      route: [String, Object],
-      disabled: Boolean
+  components: { ElTooltip },
+
+  props: {
+    index: {
+      default: null,
+      validator: val => typeof val === 'string' || val === null
     },
-    computed: {
-      active() {
-        return this.index === this.rootMenu.activeIndex;
-      },
-      hoverBackground() {
-        return this.rootMenu.hoverBackground;
-      },
-      backgroundColor() {
-        return this.rootMenu.backgroundColor || '';
-      },
-      activeTextColor() {
-        return this.rootMenu.activeTextColor || '';
-      },
-      textColor() {
-        return this.rootMenu.textColor || '';
-      },
-      mode() {
-        return this.rootMenu.mode;
-      },
-      itemStyle() {
-        const style = {
-          color: this.active ? this.activeTextColor : this.textColor
-        };
-        if (this.mode === 'horizontal' && !this.isNested) {
-          style.borderBottomColor = this.active
-            ? (this.rootMenu.activeTextColor ? this.activeTextColor : '')
-            : 'transparent';
-        }
-        return style;
-      },
-      isNested() {
-        return this.parentMenu !== this.rootMenu;
+    route: [String, Object],
+    disabled: Boolean
+  },
+  computed: {
+    active() {
+      return this.index === this.rootMenu.activeIndex;
+    },
+    hoverBackground() {
+      return this.rootMenu.hoverBackground;
+    },
+    backgroundColor() {
+      return this.rootMenu.backgroundColor || '';
+    },
+    activeTextColor() {
+      return this.rootMenu.activeTextColor || '';
+    },
+    textColor() {
+      return this.rootMenu.textColor || '';
+    },
+    mode() {
+      return this.rootMenu.mode;
+    },
+    itemStyle() {
+      const style = {
+        color: this.active ? this.activeTextColor : this.textColor
+      };
+      if (this.mode === 'horizontal' && !this.isNested) {
+        style.borderBottomColor = this.active
+          ? this.rootMenu.activeTextColor
+            ? this.activeTextColor
+            : ''
+          : 'transparent';
       }
+      return style;
     },
-    methods: {
-      onMouseEnter() {
-        if (this.mode === 'horizontal' && !this.rootMenu.backgroundColor) return;
-        this.$el.style.backgroundColor = this.hoverBackground;
-      },
-      onMouseLeave() {
-        if (this.mode === 'horizontal' && !this.rootMenu.backgroundColor) return;
-        this.$el.style.backgroundColor = this.backgroundColor;
-      },
-      handleClick() {
-        if (!this.disabled) {
-          this.dispatch('ElMenu', 'item-click', this);
-          this.$emit('click', this);
-        }
-      }
-    },
-    mounted() {
-      this.parentMenu.addItem(this);
-      this.rootMenu.addItem(this);
-    },
-    beforeDestroy() {
-      this.parentMenu.removeItem(this);
-      this.rootMenu.removeItem(this);
+    isNested() {
+      return this.parentMenu !== this.rootMenu;
     }
-  };
+  },
+  methods: {
+    onMouseEnter() {
+      if (this.mode === 'horizontal' && !this.rootMenu.backgroundColor) return;
+      this.$el.style.backgroundColor = this.hoverBackground;
+    },
+    onMouseLeave() {
+      if (this.mode === 'horizontal' && !this.rootMenu.backgroundColor) return;
+      this.$el.style.backgroundColor = this.backgroundColor;
+    },
+    handleClick() {
+      if (!this.disabled) {
+        this.dispatch('ElMenu', 'item-click', this);
+        this.$emit('click', this);
+      }
+    }
+  },
+  mounted() {
+    this.parentMenu.addItem(this);
+    this.rootMenu.addItem(this);
+  },
+  beforeDestroy() {
+    this.parentMenu.removeItem(this);
+    this.rootMenu.removeItem(this);
+  }
+};
 </script>
